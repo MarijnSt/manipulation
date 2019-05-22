580fe7106fb411f175c4a353654d426188038848
MarijnSt <marijn.stammeleer@gmail.com>
2019-05-22 14:09:31 +0200

    random charachters

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Resources:
-???
+  * [Remove/change blue glow on inputfield](https://www.jotform.com/answers/942964-Changing-the-active-focus-border-color-of-the-input-field)
+  * [Enable/disable button with php](https://stackoverflow.com/questions/42338134/create-enable-and-disable-button-in-php)
+  * [Fill array with range](https://www.w3schools.com/php/func_array_range.asp)
+  * [Random number or char](https://www.w3schools.com/php/func_math_rand.asp)
+  * [Append to string](https://www.tutorialrepublic.com/faq/how-to-append-a-string-in-php.php)
+  * [Get length of array](https://www.w3schools.com/php/func_array_count.asp)
--- src/nickname.php
+++ src/nickname.php
@@ -2,10 +2,57 @@
 <html>
   <head>
     <title>String Manipulation</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:600|Pacifico&display=swap" rel="stylesheet">
   </head>
   <body>
-    <?php
-      echo "online";
-    ?>
+    <h1>Nickname Generator</h1>
+
+    <form action="" method="post">
+      <input type="text" name="nick" placeholder="Fill in a name">
+      <button type="submit" name="sub">SUBMIT</button>
+    </form>
+
+    <div id="target">
+      <?php
+        if (isset($_POST['sub'])){
+          $input = $_POST['nick'];
+
+          $rev = strrev($input);
+          echo "<p>" . $rev . "</p>";
+          $cap = strtoupper($rev);
+          echo "<p>" . $cap . "</p>";
+          $rev2 = strrev($cap);
+          echo "<p>" . $rev2 . "</p>";
+          $dash = "--" . $rev2 . "--";
+          echo "<p>" . $dash . "</p>";
+          $x = "--x" . $rev2 . "--";
+          echo "<p>" . $x . "</p>";
+
+          //array with random letters and numbers
+          $random = [];
+          $lower = range("a", "z");
+          $upper = range("A", "Z");
+          $number = range(0, 9);
+          array_push($random, $lower, $upper, $number);
+
+          //generate random number of chars
+          $result = "";
+          $numberOfChars = rand(2,4);
+          for ($i = 0; $i < $numberOfChars; $i++){
+              $decideArr = rand(0, 2);
+              $chosenArr = $random[$decideArr];
+              $length = count($chosenArr);
+              $decideChar = rand(0, $length);
+              $chosenChar = $chosenArr[$decideChar];
+              $result .= $chosenChar;
+          }
+
+          $randChars = "--x" . $result . $rev2 . "--";
+          echo "<p>" . $randChars . "</p>";
+        }
+      ?>
+    </div>
+
   </body>
 </html>
